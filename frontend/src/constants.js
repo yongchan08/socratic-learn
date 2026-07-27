83f101b4f48e843e7fa71ea144c6864d53abe21c
Seoneui <ellieella2031@gmail.com>
2026-07-27 10:58:40 +0900

    delete: 관리자 페이지 삭제

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -6,10 +6,8 @@ export const MAX_ATTEMPTS_PER_QUESTION = 3;
 export const MAX_PDF_BYTES = 25 * 1024 * 1024;
 export const ACTIVE_SESSION_KEY = "socratic_tutor_active_session_id";
 export const ACTIVE_COURSE_KEY = "socratic_tutor_active_course_id";
-export const ADMIN_AUTH_KEY = "socratic_tutor_admin_authenticated";
 export const BACKGROUND_VOLUME_KEY = "socratic_tutor_background_music_volume";
 export const EFFECT_VOLUME_KEY = "socratic_tutor_effect_volume";
-export const ADMIN_PASSWORD = "1004";
 
 export const initialForm = {
   difficulty: "normal",
